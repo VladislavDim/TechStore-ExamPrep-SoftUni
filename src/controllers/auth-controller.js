b889b4573c79036f7e0916ac0a8ded6351d3cb0d
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-02-13 08:21:51 +0200

    Add post register action

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -6,4 +6,10 @@ authController.get('/register', (req, res) => {
     res.render('auth/register');
 });
 
+authController.post('/register', (req, res) => {
+    const userData = req.body;
+    console.log(userData);
+    res.redirect('/auth/register');
+});
+
 export default authController;
