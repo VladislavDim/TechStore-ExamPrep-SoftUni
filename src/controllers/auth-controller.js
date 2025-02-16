b3fac376daf3bc4a530d6acdf840e412f370e0b7
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-02-16 09:11:32 +0200

    Add get logout action

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -13,7 +13,7 @@ authController.post('/login', async (req, res) => {
 
     const token = await authService.login(email, password);
 
-    res.cookie(AUTH_COOKIE_NAME, token);
+    res.cookie(AUTH_COOKIE_NAME, token, { httpOnly: true });
     res.redirect('/');
 });
 
@@ -25,7 +25,12 @@ authController.post('/register', async (req, res) => {
     const userData = req.body;
     const token = await authService.register(userData);
 
-    res.cookie(AUTH_COOKIE_NAME, token);
+    res.cookie(AUTH_COOKIE_NAME, token, { httpOnly: true });
+    res.redirect('/');
+});
+
+authController.get('/logout', (req, res) => {
+    res.clearCookie(AUTH_COOKIE_NAME);
     res.redirect('/');
 });
 
