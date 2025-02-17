8dbbe6994dd20156bd274aabe6721122adb26f3f
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-02-17 23:52:45 +0200

    Add error handling

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authService from "../services/auth-service.js";
 import { AUTH_COOKIE_NAME } from "../config.js";
 import { isAuth } from "../middlewares/auth-middleware.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
 
 const authController = Router();
 
@@ -24,10 +25,15 @@ authController.get('/register', (req, res) => {
 
 authController.post('/register', async (req, res) => {
     const userData = req.body;
-    const token = await authService.register(userData);
 
-    res.cookie(AUTH_COOKIE_NAME, token, { httpOnly: true });
-    res.redirect('/');
+    try {
+        const token = await authService.register(userData);
+
+        res.cookie(AUTH_COOKIE_NAME, token, { httpOnly: true });
+        res.redirect('/');
+    } catch (err) {
+        res.render('auth/register', { error: getErrorMessage(err) });
+    }
 });
 
 authController.get('/logout', isAuth, (req, res) => {
