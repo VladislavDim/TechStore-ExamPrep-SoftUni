a536713461666cde4d7d8f8d7788e52d247b834c
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-02-28 00:02:22 +0200

    Add authentication validation to profile page access.

--- src/controllers/profile-controller.js
+++ src/controllers/profile-controller.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import profileService from "../services/profile-service.js";
+import { isAuth } from "../middlewares/auth-middleware.js";
 
 const profileController = Router();
 
-profileController.get('/:username', async (req, res) => {
+profileController.get('/:username', isAuth, async (req, res) => {
     const createdOffers = await profileService.getOffersCreatedByUser(req.user.id);
     const preferredOffers = await profileService.getPreferredOffersByUser(req.user.id);
 
